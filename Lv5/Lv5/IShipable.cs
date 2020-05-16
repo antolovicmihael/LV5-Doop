acb546287c4f8b70e549c3cc5c747035fe108cb4
MIHAEL ANTOLOVIC <antolovicmihael@gmail.com>
2020-05-16 20:25:12 +0200

    Zadatak 1

---
+++ Lv5/Lv5/IShipable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lv5
+{
+    interface IShipable
+    {
+        double Price { get; }
+        double Weight { get; }
+        string Description(int depth = 0);
+    }
+}
---
+++ Lv5/Lv5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lv5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
